70d44b0385cd6ab712146dacb837dbdfd025405f
DeveloperNaim <webdevelopernaim@gmail.com>
2023-05-03 15:09:21 +0600

    starting

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Typing Script JS
+import Typed from './src/typed.js';
+var typed = new Typed('.typing', {
+  strings: ['Web Designer', 'Web Developer', 'Youtuber', 'Blogger'],
+  typeSpeed: 80,
+  backSpeed: 80,
+  loop: true,
+});
